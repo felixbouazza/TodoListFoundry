39eb455cef2c2e64e01fd407d6031b94a86c1ea2
Félix Bouazza <fbouazza.dev@gmail.com>
2025-02-11 09:55:44 +0100

    fix build

--- script/TodoList.sol
+++ script/TodoList.sol
@@ -2,17 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
-import {Counter} from "../src/Counter.sol";
+import {TodoList} from "../src/TodoList.sol";
 
-contract CounterScript is Script {
-    Counter public counter;
+contract TodoListScript is Script {
+    TodoList public todoList;
 
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
 
-        counter = new Counter();
+        todoList = new TodoList();
 
         vm.stopBroadcast();
     }
